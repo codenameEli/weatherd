7ab7c09491a2a47ce42d8bbef2b3b0131ae29a72
Tim Dalbey <timdalbey13@gmail.com>
2024-10-18 20:02:33 -0400

    improve daily summary with sunrise and sunset information

--- src/components/DailySummary.tsx
+++ src/components/DailySummary.tsx
@@ -1,7 +1,9 @@
+import moment from 'moment';
 import { useEffect, useState } from 'react';
 
 function DailySummary({weatherAPI, reverseGeocodeAPI}) {
 	const [summary, setSummary] = useState(null);
+	const [sunriseSunset, setSunriseSunset] = useState(null);
 
 	useEffect(() => {
     if (
@@ -18,8 +20,11 @@ function DailySummary({weatherAPI, reverseGeocodeAPI}) {
 							data.results.length > 0
 						) {
 							weatherAPI.get(position.coords.latitude, position.coords.longitude).then((results) => {
-                const summary = `Today has a high of ${Math.round(results.main.temp_max)} & a low of ${Math.round(results.main.temp_min)}.`
+                const summary = `Today has a high of ${Math.round(results.main.temp_max)}° & a low of ${Math.round(results.main.temp_min)}°.`
 								setSummary(summary);
+                
+                const sunriseSunset = `Sunrise is at ${moment(results.sys.sunrise).format('LT')} and sunset is at ${moment(results.sys.sunset).format('LT')}.`
+								setSunriseSunset(sunriseSunset);
 							});
 						}
 				})
@@ -29,9 +34,8 @@ function DailySummary({weatherAPI, reverseGeocodeAPI}) {
 
 	return <div className='daily-summary text-left max-w-6xl'>
     <h3 className='tex font-extrabold text-md'>Daily Summary</h3>
-    <p className=''>
-      {summary}
-    </p>
+    <p className=''>{summary}</p>
+    <p className=''>{sunriseSunset}</p>
   </div>;
 }
 
