0cca413ef0aecde1930cc562ad178fdc050176c9
Tim Dalbey <timdalbey13@gmail.com>
2024-10-19 22:55:37 -0400

    revert pwa test

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import CurrentCondition from "./components/CurrentCondition"
 import CurrentDate from "./components/CurrentDate"
 import CurrentLocation from "./components/CurrentLocation"
@@ -12,9 +13,33 @@ const reverseGeocodeAPI = new ReverseGeocodeAPI('AIzaSyAKnSRAB-vHYHrlgwvPje8IGlb
 const weatherAPI = new WeatherAPI('ab1272725b276e1dae8ab5a260d08d82');
 
 function App() {
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    if (
+			weatherAPI &&
+			reverseGeocodeAPI &&
+			navigator.geolocation
+		) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const location = reverseGeocodeAPI.get(position.coords.latitude, position.coords.longitude);
+
+				location.then((data) => {
+						if (
+							data.results &&
+							data.results.length > 0
+						) {
+							weatherAPI.get(position.coords.latitude, position.coords.longitude).then((results) => {
+								setColor(weatherAPI.translateWeatherCodeToColor(results.current.weather_code));
+							});
+						}
+				})
+			});
+    }
+  }, [weatherAPI, reverseGeocodeAPI]);
 
   return (
-    <div className={`bg-blue-100 items-center flex flex-col text-center py-6 px-6 min-w-screen min-h-screen`}>
+    <div className={`bg-${color} items-center flex flex-col text-center py-6 px-6 min-w-screen min-h-screen`}>
      <CurrentLocation reverseGeocodeAPI={reverseGeocodeAPI}/>
      <CurrentDate />
      <CurrentCondition reverseGeocodeAPI={reverseGeocodeAPI} weatherAPI={weatherAPI} />
